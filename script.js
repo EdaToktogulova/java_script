c386453d9c8559ce33d3ac0f7a5ecd515fc0fc97
Eda Toktogulova <edatoktogulova2003@gmail.com>
2022-02-09 14:37:11 +0600

    new

--- script.js
+++ script.js
@@ -7,3 +7,14 @@ let example3Button = document.getElementById('example3');
 example3Button.addEventListener('click', function() {
    alert("Bye!");
 });
+
+let example4Button = document.querySelector('#example4');
+example4Button.addEventListener('click', function(){
+   example4Button.textContent = "Nice to meet you";
+});
+
+let example5Div = document.querySelector('#example5');
+example5Div.addEventListener('click', function(){
+   alert(example5Div.textContent.length);
+});
+
